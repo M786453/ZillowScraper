c8dccc3901b8bd99e342a0470e92d606dd040905
Ahtesham Sarwar <57073399+M786453@users.noreply.github.com>
2023-09-08 11:34:58 +0500

    zillow scrapper class created. function for retrieving listing details done

---
+++ zillow.py
@@ -0,0 +1,32 @@
+import requests
+import json
+
+class zillow:
+
+    def getListingDetails(self):
+
+        LISTING_URL = "https://www.zillow.com/search/GetSearchPageState.htm?searchQueryState=%7B%22pagination%22%3A%7B%7D%2C%22mapBounds%22%3A%7B%22north%22%3A36.81261568046124%2C%22east%22%3A-91.44158859375%2C%22south%22%3A25.48670686898427%2C%22west%22%3A-108.71209640625%7D%2C%22usersSearchTerm%22%3A%22TX%22%2C%22mapZoom%22%3A6%2C%22regionSelection%22%3A%5B%7B%22regionId%22%3A54%2C%22regionType%22%3A2%7D%5D%2C%22isMapVisible%22%3Atrue%2C%22category%22%3A%22cat2%22%2C%22filterState%22%3A%7B%22price%22%3A%7B%22min%22%3A200000%7D%2C%22isCondo%22%3A%7B%22value%22%3Afalse%7D%2C%22isForSaleForeclosure%22%3A%7B%22value%22%3Afalse%7D%2C%22isApartment%22%3A%7B%22value%22%3Afalse%7D%2C%22isMultiFamily%22%3A%7B%22value%22%3Afalse%7D%2C%22monthlyPayment%22%3A%7B%22min%22%3A1078%7D%2C%22isAllHomes%22%3A%7B%22value%22%3Atrue%7D%2C%22sortSelection%22%3A%7B%22value%22%3A%22pricea%22%7D%2C%22isAuction%22%3A%7B%22value%22%3Afalse%7D%2C%22isNewConstruction%22%3A%7B%22value%22%3Afalse%7D%2C%22isLotLand%22%3A%7B%22value%22%3Afalse%7D%2C%22isTownhouse%22%3A%7B%22value%22%3Afalse%7D%2C%22isManufactured%22%3A%7B%22value%22%3Afalse%7D%2C%22isComingSoon%22%3A%7B%22value%22%3Afalse%7D%2C%22isApartmentOrCondo%22%3A%7B%22value%22%3Afalse%7D%2C%22isForSaleByAgent%22%3A%7B%22value%22%3Afalse%7D%7D%2C%22isListVisible%22%3Atrue%7D&wants={%22cat2%22:[%22mapResults%22]}&requestId=2"
+
+        headers = {
+                    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.5845.141 Safari/537.36",
+                    "Accept": "*/*"
+                    }
+
+        listing_response = requests.get(LISTING_URL, headers=headers)
+
+        listing_data = json.loads(listing_response.text)["cat2"]["searchResults"]["mapResults"]
+        
+        counter = 1
+
+        for property in listing_data:
+
+            try:
+                print("ZPID#"+ str(counter)+":", property["zpid"])
+                counter +=1 
+            except Exception as e:
+                print(str(e))
+
+
+zillow().getListingDetails()
+
+
